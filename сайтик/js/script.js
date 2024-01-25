63093b7c214a42cc66724a1cf87c578a516a3081
exhaustedkid <118588209+lyrickors@users.noreply.github.com>
2024-01-25 12:21:35 +0700

    Update script.js

--- сайтик/js/script.js
+++ сайтик/js/script.js
@@ -1,1 +1,15 @@
-в разработке
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function generateData() {
+    let temperature = getRandomNumber(0, 120);
+    let pressure = getRandomNumber(900, 1100);
+    let humidity = getRandomNumber(40, 60);
+
+    document.getElementById('weather-info').innerText = `Temperature: ${temperature}°C, Pressure: ${pressure} hPa, Humidity: ${humidity}%`;
+  }
+
+  generateData();
+
+  setInterval(generateData, 3000);
